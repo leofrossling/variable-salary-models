5a7785feff3f8affccffc63ce2b8bc824cd5f130
Henrik Nordlund <henrik.nordlund@devport.se>
2024-11-07 11:15:55 +0100

    Added variable month length and off-days

--- bonusmodel_v1.py
+++ bonusmodel_v1.py
@@ -1,9 +1,71 @@
 import json
 import os
 import math
+import calendar as cal
 
 from deltek import read_timetables
+from holiday_api import get_easter_holidays, get_ascension_day, get_midsummers_eve
 
+def day_to_string(date):
+    match date.weekday():
+        case 0:
+            return "Monday"
+        case 1:
+            return "Tuesday"
+        case 2:
+            return "Wednesday"
+        case 3:
+            return "Thursday"
+        case 4:
+            return "Friday"
+        case 5:
+            return "Saturday"
+        case 6:
+            return "Sunday"
+        case _:
+            return "hmm"
+
+def get_monthly_billable_hours_by_year(year: int):
+    goodfriday, eastermonday = get_easter_holidays(year)
+    ascension_day = get_ascension_day(year)
+    calendar = cal.Calendar()
+    res = []
+    for i in range(12):
+        month = i + 1
+        count = 0
+        for date in calendar.itermonthdates(year, month):
+            if date.month != month or date.year != year:
+                continue
+            if date.weekday() == 5 or date.weekday() == 6:
+                continue
+            if date.month == 1 and date.day == 1:
+                # New years day
+                continue
+            if date.month == 1 and date.day == 6:
+                # Trettondagen
+                continue
+            if date.isoformat() in [goodfriday, eastermonday]:
+                # Påsken
+                continue
+            if date.month == 5 and date.day == 1:
+                # First of May
+                continue
+            if date.isoformat() == ascension_day:
+                # Ascension Day
+                continue
+            if date.month == 6 and date.day == 6:
+                # Nationaldagen
+                continue 
+            if date.month == 12 and date.day in [24, 25, 26, 31]:
+                # Jul och nyårsafton
+                continue
+            count += 1
+        if month == 6:
+            # Midsommarafton
+            count -= 1
+        res.append(count * 8)
+        
+    return res
 
 # return: interntid, rtotalgrundande, rlingrundande, okänt
 def daily_result(record: dict, unknown: dict):
@@ -62,9 +124,9 @@ def daily_result(record: dict, unknown: dict):
         
     if jobnumber not in unknown:
         unknown[jobnumber] = {"description": jobname, "activities": {}}
+    unknown[jobnumber]["activities"][activitynumber]["hours"] += hours
     if activitynumber not in unknown[jobnumber]["activities"]:
         unknown[jobnumber]["activities"][activitynumber] = {"description": record["entrytext"], "hours": 0}
-    unknown[jobnumber]["activities"][activitynumber]["hours"] += hours
 
     res['type'] = 'unknown'
     return "unknown", res
@@ -80,6 +142,8 @@ def print_report(report):
             print(f"  Rtotal hour bank={report[year][month]['rtot_bank']:.1f}h")
             print(f"  Received Rlin={report[year][month]['Rlin']:.1f}h")
             print(f"  Received Rlon={report[year][month]['Rlön']:.1f}h")
+            print("")
+
             grouped_month = {}
             for record in report[year][month]['records']:
                 jobnum = record['jobnumber']
@@ -110,8 +174,12 @@ def print_report(report):
 
 def calculate_years(records: dict[int, dict[int, dict[int, list[dict]]]]):
 
-    rtot_val = int(os.environ["RTOTAL"])
-    rlin_val = int(os.environ["RLIN"])
+    rtot_val = 2000
+    rlin_val = 40
+    if "RTOTAL" in os.environ:
+        rtot_val = int(os.environ["RTOTAL"])
+    if "RLIN" in os.environ:
+        rlin_val = int(os.environ["RLIN"])
     rlon_val = math.ceil(rlin_val + rtot_val / 38)
 
     print("")
@@ -122,13 +190,12 @@ def calculate_years(records: dict[int, dict[int, dict[int, list[dict]]]]):
     years = {}
     report = {}
 
-    total_hours_per_month = 168  # TODO: Temp Simplification
     extra_bonus_hours_from_december = 0
     for year in records.keys():
         if year not in report:
             report[year] = {}
         year_bonus_hours = 0
-
+        hours_by_month: list[int] = get_monthly_billable_hours_by_year(int(year))
         rtot_bank = extra_bonus_hours_from_december # extra hours not compensated are carried into the next year
         rtot_count = 0.0
         rlin_count = 0
@@ -139,7 +206,7 @@ def calculate_years(records: dict[int, dict[int, dict[int, list[dict]]]]):
                     'records':[]
                 }
             rlon_billable = 0
-            monthly_billable_hours = 0
+            monthly_billed_hours = 0
             monthly_bonus_hours = 0
             monthly_non_bonus_hours = 0
             monthly_unknown_hours = 0
@@ -159,7 +226,7 @@ def calculate_years(records: dict[int, dict[int, dict[int, list[dict]]]]):
                         case "unknown":
                             monthly_unknown_hours += parsed_record['hours']
                         case "billable":
-                            monthly_billable_hours += parsed_record['hours']
+                            monthly_billed_hours += parsed_record['hours']
                             monthly_bonus_hours += parsed_record['hours']
                             day_is_billable="Yes"
                         case "vacation":
@@ -190,11 +257,12 @@ def calculate_years(records: dict[int, dict[int, dict[int, list[dict]]]]):
                 elif day_is_billable == "Yes":
                     last_day_was_billable=True
 
-            rtotal_required_hours = total_hours_per_month - vab_equivalent_hours
+            hour_for_month = hours_by_month[int(month) - 1]
+            rtotal_required_hours = hour_for_month - vab_equivalent_hours
             if monthly_bonus_hours >= rtotal_required_hours:
                 rtot_increment = 1
                 if vab_equivalent_hours > 0:
-                    rtot_increment = min(1.0, monthly_bonus_hours / total_hours_per_month)
+                    rtot_increment = min(1.0, monthly_bonus_hours / hour_for_month)
                 rtot_count += 1 
                 excess_bonus_hours = monthly_bonus_hours - rtotal_required_hours
                 report[year][month]['Rtotal'] = 'True, with %.1fh hours extra' % (excess_bonus_hours)
@@ -212,9 +280,9 @@ def calculate_years(records: dict[int, dict[int, dict[int, list[dict]]]]):
 
             rlin_threshold = 130
             if rlin_vacation_hours > 0:
-                rlin_threshold = max(min(130,monthly_billable_hours / total_hours_per_month * 130),0) 
-            h_lin = max(monthly_billable_hours - rlin_threshold, 0)
-            h_lon = max(monthly_billable_hours + rlon_vacation_hours + rlon_billable - 130, 0)
+                rlin_threshold = max(min(130,monthly_billed_hours / hour_for_month * 130),0) 
+            h_lin = max(monthly_billed_hours - rlin_threshold, 0)
+            h_lon = max(monthly_billed_hours + rlon_vacation_hours + rlon_billable - 130, 0)
             rlin_count += h_lin
             rlon_count += h_lon
             report[year][month]['Rlin'] = h_lin
@@ -223,10 +291,10 @@ def calculate_years(records: dict[int, dict[int, dict[int, list[dict]]]]):
             
             
         # Calculate year bonuses
-        yearly_hours = total_hours_per_month * 12
+        yearly_hours = sum(hours_by_month)
         if year_bonus_hours >= yearly_hours - 40:
             if year_bonus_hours >= yearly_hours:
-                rtot_count = 12 + min(1, (year_bonus_hours - yearly_hours) / total_hours_per_month)
+                rtot_count = 12 + min(1, (year_bonus_hours - yearly_hours) / 168)
                 report[year]['Rtotal'] = "More bonus hours than hours in year. Hard at work!"
                 extra_bonus_hours_from_december = 0 # extra bonus hours only carry over if not paid out by excess hours previous year
             else:
@@ -243,7 +311,8 @@ def calculate_years(records: dict[int, dict[int, dict[int, list[dict]]]]):
         print(f"  {rtot_count=}st")
         print(f"  {rlin_count=:.1f}h")
         print(f"  {rlon_count=:.1f}h")
-        print(f"  {json.dumps(unknown, indent=2)}")
+        if len(unknown.keys()) > 0:
+            print(f"  {json.dumps(unknown, indent=2)}")
         print("")
         print(f"  Total current: {rtot_count:.1f}*{rtot_val} + {rlin_count:.1f}*{rlin_val}=")
         print(f"                 {rtot_count*rtot_val + rlin_count*rlin_val:.0f} kr")
@@ -287,3 +356,8 @@ def the_main_program():
 
 if __name__ == "__main__":
     the_main_program()
+    # for year in range(20):
+    #     print(f"Year: {2010 + year}")
+    #     total = sum(get_monthly_billable_hours_by_year(2010 + year))
+    #     print(f"Monthly avg for {2024 + year}={total * 8 / 12:.1f}")
+        
---
+++ holiday_api.py
@@ -0,0 +1,78 @@
+import requests
+import os
+import json
+
+
+def fetch_holidays_for_year(year) -> dict:
+    file_name = f"holidays_{year}.json"
+    try:
+        with open(file_name, "r") as fp:
+            data = json.load(fp)
+            return data
+    except Exception:
+        pass
+
+    if "API_NINJA_KEY" not in os.environ:
+        return []
+    key = os.environ["API_NINJA_KEY"]
+    
+    headers = {
+        "X-Api-Key": key,
+    }
+
+    url = f"https://api.api-ninjas.com/v1/holidays?country=SWE&year={year}&type=public_holiday"
+
+    print("Fetching holiday dates from API")
+    response = requests.get(url, headers=headers, timeout=30)
+
+    if not response.status_code == 200:
+        raise Exception(f"Error fetching timetables: {response.reason}")
+
+    holidays = response.json()
+    
+    with open(file_name, "w") as fp:
+        json.dump(holidays, fp)
+
+    return holidays
+
+def get_easter_holidays(year):
+    goodfriday = "error"
+    eastermonday = "error"
+    holidays = fetch_holidays_for_year(year)
+
+    for holiday in holidays:
+        if holiday['name'] == "Good Friday":
+            # print("Långfredag är " + holiday['date'])
+            goodfriday = holiday['date']
+        if holiday['name'] == "Easter Monday":
+            # print("Annandag påsk är " + holiday['date'])    
+            eastermonday = holiday['date']
+    
+    return goodfriday, eastermonday
+
+def get_ascension_day(year):
+    day = "error"
+    holidays = fetch_holidays_for_year(year)
+
+    for holiday in holidays:
+        if holiday['name'] == "Ascension Day":
+            day = holiday['date']
+    
+    return day
+
+def get_midsummers_eve(year):
+    day = "error"
+    holidays = fetch_holidays_for_year(year)
+
+    for holiday in holidays:
+        if holiday['name'] == "Midsummer Day":
+            day = holiday['date']
+    if day != "error":
+        parts = [int(x) for x in day.split("-")]
+        parts[2] -= 1
+        day = "-".join([str(x) for x in parts])
+    return day
+
+
+if __name__ == "__main__":
+    print(get_midsummers_eve(2025))
